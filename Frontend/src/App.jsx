d3d5949d2bfbf87ca7f03b0bcc80e89037814b80
Tushar Gupta <tushargupta8954@gmail.com>
2026-04-24 16:37:55 +0530

    Created login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import { store } from './store/store';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import ResumeBuilder from './pages/ResumeBuilder';
+import PrivateRoute from './components/PrivateRoute';
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <Provider store={store}>
+      <Router>
+        <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+          <Toaster position="top-right" />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            } />
+            <Route path="/builder/:id?" element={
+              <PrivateRoute>
+                <ResumeBuilder />
+              </PrivateRoute>
+            } />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
